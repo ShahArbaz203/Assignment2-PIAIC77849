6401283560ab59561a3fe32ecd96e5411eeeb040
Shaharbaz203 <64959721+ShahArbaz203@users.noreply.github.com>
2021-01-04 10:53:00 -0800

    Add files via upload

---
+++ ASSIGNMENT2 = PIAIC77849/TASK1/task1.js
@@ -0,0 +1,9 @@
+// Write a program to take “city” name as input from user. If user enters
+// “Karachi”, welcome the user like this: “Welcome to city of lights”
+
+
+
+var cityName = prompt("Enter your city name");
+if(cityName==="Karachi"){
+    alert("Welcome to the city of light")
+}
